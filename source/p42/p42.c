2cb3109a4593b71db4b7c5dea212331583154bd5
ruslan0399 <ruslan0399@gmail.com>
2018-07-03 02:11:10 +0300

    Add files via upload

--- source/p42/p42.c
+++ source/p42/p42.c
@@ -1,1 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct elem
+{
+	int i;
+	struct elem *n;
+} elem;
+
+elem *add(elem *l, int v)
+{
+	elem *t = malloc(sizeof(elem)), *p = l;
+	t->n = NULL;
+	t->i = v;
+
+	if(l == NULL) return t;
+	while(l->n != NULL) l = l->n;
+	l->n = t;
+
+	return p;
+}
+
+int check_merge(elem *a, elem *b)
+{
+	elem *pa = a, *pb = b;
+
+	while(pa != NULL && pb != NULL)
+	{
+		if(pa->i < pb->i)
+			return 0;
+		else if(pa->i > pb->i)
+			pb = pb->n;
+		else
+		{
+			pa = pa->n;
+			pb = pb->n;
+		}
+	}
+
+	return pa == NULL;
+}
+
+int main()
+{
+	elem *a = NULL, *b = NULL;
+
+	int v;
+	while(scanf("%d", &v) != EOF) a = add(a, v);
+	while(scanf("%d", &v) != EOF) b = add(b, v);
+
+	printf("%d\n", check_merge(a, b));
+}
