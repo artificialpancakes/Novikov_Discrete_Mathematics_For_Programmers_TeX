8fca4840342a1bf746d11fd06967e8be6305769a
ruslan0399 <ruslan0399@gmail.com>
2018-07-02 15:03:20 +0300

    Add files via upload

--- source/p39/p39.c
+++ source/p39/p39.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	int s = pow(2, n), m[s];
+	for(int i = 0; i < s; ++i)
+		m[i] = 0;
+
+	for(int i = 0; i < pow(2, n); ++i)
+	{
+		int t = i, p = 1;
+		while(t)
+		{
+			m[i] += t % 2 * p;
+			t /= 2;
+			p *= 10;
+		}
+	}
+
+	for(int i = 0; i < s; ++i)
+		printf("%d\n", m[i]);
+}
