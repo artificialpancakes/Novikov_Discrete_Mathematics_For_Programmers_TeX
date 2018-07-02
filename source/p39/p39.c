b5b3d5d783244e10be8a81b169642b5f3fa9d475
ruslan0399 <ruslan0399@gmail.com>
2018-07-03 02:10:16 +0300

    Add files via upload

--- source/p39/p39.c
+++ source/p39/p39.c
@@ -7,19 +7,8 @@ int main()
 	scanf("%d", &n);
 
 	int s = pow(2, n), m[s];
-	for(int i = 0; i < s; ++i)
-		m[i] = 0;
-
 	for(int i = 0; i < pow(2, n); ++i)
-	{
-		int t = i, p = 1;
-		while(t) //код очередного подмножества
-		{
-			m[i] += t % 2 * p;
-			t /= 2;
-			p *= 10;
-		}
-	}
+		m[i] = i; //код очередного подмножества
 
 	for(int i = 0; i < s; ++i)
 		printf("%d\n", m[i]);
