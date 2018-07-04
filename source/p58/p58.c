66341fa3172a6bf07a9b038107e26bdb860ebe29
ruslan0399 <ruslan0399@gmail.com>
2018-07-05 01:25:38 +0300

    Add files via upload

--- source/p58/p58.c
+++ source/p58/p58.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+	
+int main()
+{
+	int n;
+	scanf("%d", &n);
 
+	int R[n][n], T[n][n];
+	for(int i = 0; i < n; ++i)
+	{
+		for(int j = 0; j < n; ++j)
+		{
+			int t;
+			scanf("%d", &t);
+			R[i][j] = t;
+		}
+	}
+
+	for(int i = 0; i < n; ++i)
+	{
+		for(int j = 0; j < n; ++j)
+		{
+			for(int k = 0; k < n; ++k)
+				T[j][k] = R[j][k] | (R[j][i] & R[i][k]);
+		}
+		for(int a = 0; a < n; ++a)
+			for(int j = 0; j < n; ++j)
+				R[a][j] = T[a][j];
+	}
+
+	printf("\n");
+	for(int i = 0; i < n; ++i)
+	{
+		for(int j = 0; j < n; ++j)
+			printf("%d ", T[i][j]);
+		printf("\n");
+	}
+}
