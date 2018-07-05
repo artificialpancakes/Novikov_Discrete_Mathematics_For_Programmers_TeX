78f6e3607dc1b83e5fbda9e9a8eb69492bcbc23c
ruslan0399 <ruslan0399@gmail.com>
2018-07-05 23:07:38 +0300

    Add files via upload

--- source/p137/p137.c
+++ source/p137/p137.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
 
+int main()
+{
+	int k, n, res = 0;
+	scanf("%d%d", &k, &n);
+	int f[k][n], x[n];
+	
+	for(int i = 0; i < k; ++i)
+	{
+		for(int j = 0; j < n; ++j)
+		{
+			int t;
+			scanf("%d", &t);
+			f[i][j] = t;
+		}
+	}
+
+	for(int i = 0; i < n; ++i)
+	{
+		int t;
+		scanf("%d", &t);
+		x[i] = t;
+	}
+
+	for(int i = 0; i < k; ++i)
+	{
+		int flag = 0;
+		for(int j = 0; j < n; ++j)
+		{
+			if(f[i][j] != x[j])
+				flag = 1;
+		}
+		if(flag) continue;
+		res = 1;
+	}
+
+	printf("%d\n", res);
+}
