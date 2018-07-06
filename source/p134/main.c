a4b2fdc2f53a494358a78c2730d6e26d36595fee
artificialpuncakes <starcheussd@gmail.com>
2018-07-07 01:56:32 +0300

    final version

---
+++ source/p134/main.c
@@ -0,0 +1,9 @@
+int calc_cortege_index(int n, int *x) {
+  int d = 0; /* начальное значение индекса */
+  for (int i = 0; i < n; i++) {
+    d = d << 2;
+    if (x[i] == 1) {
+      d++; /* добавляем 1, если нужнo~*/
+    }
+  }
+}
