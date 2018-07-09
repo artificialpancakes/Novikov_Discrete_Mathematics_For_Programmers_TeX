588e843c3c495966c3212cee8032553415b49697
ruslan0399 <ruslan0399@gmail.com>
2018-07-09 19:10:30 +0300

    Add files via upload

--- source/p176/p176.c
+++ source/p176/p176.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+bool method_of_resolution(set C)
+{
+	set M = C;
+	while(1)
+	{
+		elem c1, c2;
+		Choose(M, c1, c2, p1, p2, o);
+		if(empty(c1) && empty(c2))
+			return false;
+		elem c = R(c1, c2, p1, p2, o);
+		add_elem(M, c);
+		if(empty(c)) break;
+	}
+	return true;
+}
+
+int main()
+{
+	
+}
