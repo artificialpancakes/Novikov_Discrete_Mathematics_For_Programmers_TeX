9e3c64d52d8336b50ac137de6ed5839aa1b28363
artificialpuncakes <starcheussd@gmail.com>
2018-07-11 02:19:14 +0300

    polish p134

--- source/p134_final/main.c
+++ source/p134_final/main.c
@@ -1,9 +1,9 @@
-int calc_cortege_index(int n, int *x) {
+int calc_cortege_number(int n, int *x) {
   int d = 0; /* начальное значение индекса */
   for (int i = 0; i < n; i++) {
     d = d << 2;
     if (x[i] == 1) {
-      d++; /* добавляем 1, если нужнo~*/
+      d++; /* добавляем 1, если нужнo */
     }
   }
 }
