96eb01e7ca29653ae2214dc7a1d34864551b8c34
ruslan0399 <ruslan0399@gmail.com>
2018-07-10 20:02:29 +0300

    Add files via upload

--- source/p229/algorithm_6.3.c
+++ source/p229/algorithm_6.3.c
@@ -1,1 +1,61 @@
+#include <stdio.h>
+#include <string.h>
 
+char D[100][50];
+int d;
+
+int Length(int i)
+{
+	return strlen(D[i]);
+}
+
+int FD(int k, char *f)
+{
+	int l = 0, p = 0;
+	for(int i = 0; i < d; ++i)
+	{
+		int m = Length(i);
+		char s[m];
+		for(int j = 0; j < m; ++j)
+			s[j] = f[k + j];
+		if((m > l) && (strcmp(D[i], s) == 0))
+		{
+			p = i;
+			l = m;
+		}
+	}
+	return p;
+}
+
+void pack(int n, char *f)
+{
+	D[0][0] = '\0';
+	d = 0;
+	int k = 0;
+	while(k < n)
+	{
+		int p = FD(k, f);
+		int l = Length(p);
+		printf("%d %c\n", p, f[k + l]);
+		d = d + 1;
+		strcpy(D[d], D[p]);
+		D[d][l] = f[k + l];
+		k = k + l + 1;
+	}
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	char f[n];
+	for(int i = 0; i < n; ++i)
+	{
+		char t;
+		scanf("%c", &t);
+		f[i] = t;
+	}
+
+	pack(n, f);
+}
