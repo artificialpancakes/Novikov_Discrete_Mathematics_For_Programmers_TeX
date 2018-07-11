86a2b8b6598c50c6408ed816f51d8dd22d27276f
dandachok <dandachok@yandex.ru>
2018-07-11 12:41:25 +0300

    p216

--- source/p216/p216.c
+++ source/p216/p216.c
@@ -2,40 +2,81 @@
 #include<math.h>
 #include<stdlib.h>
 
+double modul(double a){
+	if(a > 0)
+		return a;
+	else
+		return (-1) * a;
+}
+
 int Med(double *P, int b, int e){
 	double d;
-	double Se;
 	double Sb = 0;
 	for(int i = b; i < e; i++){
-		Sb = Sb + P[i];
+		Sb += P[i];
 	}
-	Se = P[e];
+	double Se = P[e];
+//	printf("%lf %lf/n", Sb, Se);
 	int m = e;
 	do{
-		d = abs(Sb - Se);
+//		printf("%.2lf %.2lf ", Sb, Se);
+		d = modul(Sb - Se);
+//		printf("%.2lf \n", d);
 		m--;
 		Sb = Sb - P[m];
 		Se = Se + P[m];
-	}while(abs(Sb - Se) >= d);
+	}while(modul(Sb - Se) < d);
 	return m;
 }
 
-void Fano(double *P, int **C, int b, int e, int k){
+void Fano(double *P, int **C, int b, int e, int k, int n){
 	int *C_arr = (int*)C;
 	if(e > b){
-		k++;
 		int m = Med(P, b, e);
 		for(int i = b; i <= e; i++){
 			if(i > m)
-				C_arr[k * e + i] = 1;
+				C_arr[i * n + k] = 1;
 			else
-				C_arr[k * e + i] = 0;
+				C_arr[i * n + k] = 0;
 		}
-		Fano(P, (int**)C, b, m, k);
-		Fano(P, (int**)C, m + 1, e, k);
+		Fano(P, (int**)C, b, m, k + 1, n);
+		Fano(P, (int**)C, m + 1, e, k + 1, n);
 	}
+}
+void fun(double* P,int** C, int n){
+	int *C_arr = (int*)C;
+	double Sum = 0;
+	printf("P_i\tC[i]\tl_i\tP_i*l_i\n");
+	for(int i = 0; i < n; i++){
+		printf("%.2lf:\t", P[i]);
+		for(int j = 0; j < n; j++){
+			if(C_arr[i * n + j] != 2)
+				printf("%d", C_arr[i * n + j]);
+			else{
+				P[i] *= (double)j;
+				Sum += P[i];
+				printf("\t%d\t%.2lf", j, P[i]);
+				break;
+			}
+		}
+		printf("\n");
+	}
+	printf("l(P)\t\t\t%.2lf\n", Sum);
 }
 
 int main(){
+	int n;
+	scanf("%d", &n);
+	double P[n];
+	int C[n][n];
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++)
+			C[i][j] = 2;
+	}
+	for(int i = 0; i < n; i++){
+			scanf("%lf", &P[i]);
+	}
+	Fano(P, (int**)C, 0, n - 1, 0, n);
+	fun(P, (int**)C, n);
 	return 0;
 }
